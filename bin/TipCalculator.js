a305c692898e2265be543812e511941471db1900
LauKwaiFanHK <laukwaifanthbde@gmail.com>
2022-09-16 00:14:43 +0200

    Add comment to tip calculator

--- bin/TipCalculator.js
+++ bin/TipCalculator.js
@@ -1,36 +1,48 @@
 #!/usr/bin/env node
 import Inquirer from 'inquirer';
 
+// Calculate the amount of tip 
 function calculateTip(billAmount, tipPercentage) {
     let tip = 0;
     tip = billAmount * (tipPercentage / 100);
     return tip;
 }
 
+// Calculate the total amount of the bill
 function calculateTotal(billAmount, tipAmount) {
     const total = billAmount + tipAmount;
     return total;
 }
 
+// Tip calculator to ask user's questions,
+// prompt for answers and display results 
 const TipCalculator = () => {
+    // local variable to save the status of the usage of the tip calculator
     let status = 'started';
 
     Inquirer
+        // Takes an array of questions
         .prompt([
             {
                 type: 'input',
                 name: 'billAmount',
                 message: 'Bill amount? $',
                 validate(answer) {
+                    // Check if user enters valid input for bill amount,
+                    // if invalid, return an error message and ask the same question again,
+                    // bill amount must be given, can be converted to a number, non-negative value   
                     if (!answer) {
                         return 'Please enter a bill amount!'
                     } else if (isNaN(parseFloat(answer)) || isNaN(Number(answer)) || Number(answer) <= 0) {
                         return 'Bill amount must be a number!'
                     }
-                    return true
+                    return true // Return true to display the next question
                 }
             },
             {
+                // Check if user enters valid input for tip percentage,
+                // if invalid, return an error message and ask the same question again,
+                // tip % must be given, can be converted to a float, value within 0 - 100 range   
                 type: 'input',
                 name: 'tipPercentage',
                 message: 'Tip %?',
@@ -45,6 +57,7 @@ const TipCalculator = () => {
             }
         ])
         .then(answers => {
+            // Display results i.e. bill amount, tip %, tip amount, total bill amount
             const billAmount = parseFloat(answers.billAmount);
             const tipPercentage = parseFloat(answers.tipPercentage);
             const tipAmount = Number(calculateTip(billAmount, tipPercentage).toFixed(1));
@@ -53,8 +66,9 @@ const TipCalculator = () => {
             console.info('-----------------------------------------');
             console.info('Tip amount is: $', tipAmount);
             console.info('Total bill amount is: $', calculateTotal(billAmount, tipAmount));
-            status = 'ended'
+            status = 'ended' // Indicate this usage of the tip calculator has finished
             console.info('========================================');
+            // Call the tip calculator recursively once an usage has finished
             if (status === 'ended') {
                 TipCalculator();
             }
